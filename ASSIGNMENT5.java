4d9add0ee6c6a7fde77a8b84dac38e7fa6188a43
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-02-23 21:43:34 +0100

    Update ASSIGNMENT5.java

--- ASSIGNMENT5.java
+++ ASSIGNMENT5.java
@@ -3,7 +3,7 @@
  
 public class ASSIGNMENT5 {
    public static void main(String[] args) {
-        //PROGRAM TO SHOW RUNTIME ERROR...
+        //PROGRAM TO SHOW RUNTIME ERROR....
         System.out.println(1/0);
     }
     
