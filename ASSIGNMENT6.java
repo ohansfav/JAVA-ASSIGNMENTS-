ec42eef735f7ee797a6c3848d43a6892694e5ace
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-02-28 22:59:20 +0100

    Update ASSIGNMENT6.java

--- ASSIGNMENT6.java
+++ ASSIGNMENT6.java
@@ -3,7 +3,7 @@
 
 public class ASSIGNMENT6 {
     public static void main(String[] args) {
-        //To Show Logic Error..
+        //To Show Logic Error...
         System.out.println("celsius fahrenheit degree");
         System.out.println((9/5) * 35 + 32);
     }
