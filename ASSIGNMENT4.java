4d13b7a200ee2a23540dbe9d60ab195507055db7
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-05-08 14:05:58 +0100

    Update ASSIGNMENT4.java

--- ASSIGNMENT4.java
+++ ASSIGNMENT4.java
@@ -6,7 +6,7 @@
 public class ASSIGNMENT4 {
 
     public static void main(String[] args) {
-        /*....Multiple Line Comment....*/
+        /*....Multiple Line Commentt....*/
         System.out.println("welcome to csc 211 class");
         //Single Line Comment
         System.out.println("This is a block style");
